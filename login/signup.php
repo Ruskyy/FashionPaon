388ea216fa5e13c4c76d9bf33d7d13c29a16b9ec
Klekito <39529387+Klekito@users.noreply.github.com>
2018-10-29 22:28:28 +0000

    IMG upload register

--- login/signup.php
+++ login/signup.php
@@ -1,12 +1,13 @@
 <?php
-include 'php/conn.php'
+include 'php/conn.php';
+include 'php/functions.php';
 ?>
 
 <div class="card col-md-12 mx-auto" style="padding:2%">
   <div class="row col-md-2" style="margin-bottom:2%;">
     <img class="img-card-top" src="00_Recources/logo_text.png" alt="Card image cap">
   </div>
-  <form class="" method="post">
+  <form class="" method="post" enctype="multipart/form-data">
     <div class="row">
       <div class="col-md-6">
         <div class="row">
@@ -64,6 +65,7 @@ include 'php/conn.php'
         <br>
         <input class="form-control form-control-lg" type="number" name="nif" value="" placeholder="NIF" maxlength="9" size="9" required>
         <br>
+        <input type="file" class="form-control" name="image" />
       </div>
       </div>
       <br>
@@ -73,12 +75,41 @@ include 'php/conn.php'
   <?php
   if(isset($_POST['submeter'])){
     include 'php/conn.php';
-    $img_path = "image/unknown.png";
     $codpost = $_POST['codigo'].'-'.$_POST['postal'];
+
     //falta  cliente_img_path
     /*mysqli_query($conn,"INSERT INTO  cliente (  cliente_username,cliente_password, cliente_nome,	  cliente_apelido,	 cliente_datanasc,  cliente_morada,	 	  cliente_codigopostal,	 	 cliente_idpais ,	 	cliente_nif	,	 cliente_tele	,	 cliente_email	,	  cliente_tipo )
     VALUES ('$_POST[user]','$_POST[pass]','$_POST[fname]','$_POST[lname]','$_POST[data]','$_POST[morada]','$codpost','$_POST[paises]','$_POST[nif]','$_POST[tele]','$_POST[email]',0)");
     */
+
+    if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+
+      $expensions= array("jpeg","jpg","png");
+
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+
+      if(empty($errors)==true){
+        $generatedname = generateRandomString(100).'.'.$file_ext;
+        $img_path="images/uploads/".$generatedname;
+         move_uploaded_file($file_tmp,"images/uploads/".$generatedname);
+      }else{
+         echo($errors);
+      }
+    }else {
+        $img_path="images/unknown.png";
+    }
 
     $username=mysqli_fetch_array(mysqli_query($conn,"SELECT cliente_username FROM cliente WHERE cliente_username='$_POST[user]'"));
     $email=mysqli_fetch_array(mysqli_query($conn,"SELECT cliente_email FROM cliente WHERE cliente_email='$_POST[email]'"));
@@ -89,6 +120,7 @@ include 'php/conn.php'
 
       /*mysqli_query($conn,"INSERT INTO  cliente (cliente_username, cliente_password, cliente_nome, cliente_apelido, cliente_datanasc, cliente_morada, cliente_codigopostal, cliente_idpais, cliente_nif,	cliente_tele, cliente_email,	cliente_tipo )
       VALUES ('$_POST[user]','$_POST[pass]','$_POST[fname]','$_POST[lname]','$_POST[data]','$_POST[morada]','$codpost','$_POST[paises]','$_POST[nif]','$_POST[tele]','$_POST[email]',0)");*/
+
 
       mysqli_query($conn,"CALL usp_register('$_POST[user]','$_POST[pass]','$_POST[fname]','$_POST[lname]','$_POST[data]','$_POST[morada]','$codpost','$_POST[paises]','$_POST[nif]','$_POST[tele]','$_POST[email]','$img_path')");
 

7c5fcfac4d323ec1f6584f1085faff5c0c828fdd
Rusky <39529472+Ruskyy@users.noreply.github.com>
2018-11-07 16:08:57 +0000

    SQL - Importante

--- 01_SQL/fashedot.sql
+++ 01_SQL/fashedot.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.8.1
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 25-Out-2018 às 23:46
--- Versão do servidor: 10.1.16-MariaDB
--- PHP Version: 5.6.24
+-- Generation Time: 07-Nov-2018 às 17:07
+-- Versão do servidor: 10.1.33-MariaDB
+-- PHP Version: 7.2.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -19,6 +21,22 @@ SET time_zone = "+00:00";
 --
 -- Database: `fashedot`
 --
+
+DELIMITER $$
+--
+-- Procedures
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_login` (IN `username` VARCHAR(20), IN `password` VARCHAR(50))  BEGIN
+SELECT cliente_id, cliente_tipo, cliente_username, cliente_nome, cliente_apelido, cliente_datanasc, cliente_morada, cliente_codigopostal, cliente_idpais, cliente_nif, cliente_tele, cliente_email, cliente_img_path
+FROM cliente WHERE cliente_username like username AND cliente_password like password;
+END$$
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `usp_register` (IN `username` VARCHAR(20), IN `password` VARCHAR(50), IN `nome` VARCHAR(20), IN `apelido` VARCHAR(20), IN `datanasc` DATE, IN `morada` VARCHAR(100), IN `codigopostal` VARCHAR(11), IN `idpais` INT, IN `nif` INT, IN `tele` INT, IN `email` VARCHAR(100), IN `img` VARCHAR(10000))  BEGIN
+INSERT INTO cliente (cliente_username,cliente_password,cliente_nome,cliente_apelido,cliente_datanasc,cliente_morada,cliente_codigopostal,cliente_idpais,cliente_nif,cliente_tele,cliente_email,cliente_tipo,cliente_img_path)
+  VALUES (username,password,nome,apelido,datanasc,morada,codigopostal,idpais,nif,tele,email,0,img);
+END$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -83,8 +101,306 @@ CREATE TABLE `cliente` (
 --
 
 INSERT INTO `cliente` (`cliente_id`, `cliente_username`, `cliente_password`, `cliente_nome`, `cliente_apelido`, `cliente_datanasc`, `cliente_morada`, `cliente_codigopostal`, `cliente_idpais`, `cliente_nif`, `cliente_tele`, `cliente_email`, `cliente_tipo`, `cliente_img_path`) VALUES
-(8, 'Proident iste nostru', 'Pa$$w0rd!', 'Cameran Mclaughlin', 'Nita Barker', '2016-01-25', 'Anim ex numquam ratione est laboris ut voluptas quia eaque labore animi ratione id', '2345-123', 0, 0, 0, 'suvofyxuza@mailinator.com', 0, 'images/unkown.png'),
-(9, 'Suscipit ex ut nihil', 'Pa$$w0rd!', 'Dale Gillespie', 'Geraldine Hendrix', '2001-08-08', 'Quidem illum enim laboriosam id quas incidunt velit', '1212-21212', 0, 152551255, 712455, 'rehudeg@mailinator.com', 0, 'images/unkown.png');
+(11, 'teste1', 'e959088c6049f1104c84c9bde5560a13', 'Goncalo', 'Lavrador', '2017-10-26', 'Sapiente quia et laboriosam occaecat in porro enim eum ullamco et consequatur dicta esse', '34-86', 0, 75, 47, 'goxyhosi@mailinator.net', 0, ''),
+(12, 'teste2', '38851536d87701d2191990e24a7f8d4e', 'Nilton', 'Fontes', '2011-08-16', 'Harum eaque consequatur Omnis asperiores', '36-3', 0, 98, 26, 'jidesot@mailinator.com', 0, 'images/uploads/ZW3lc87l2syaeqeb8ja1qyUal4WELCXipBf9h8y033btKj845wPVQdLIvepbKP2lDkdLxZd7VSMbvBCzPgYN1VCxCwlaFdrWdnPQ.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `mes`
+--
+
+CREATE TABLE `mes` (
+  `mes_id` int(11) NOT NULL,
+  `mes_nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `mes`
+--
+
+INSERT INTO `mes` (`mes_id`, `mes_nome`) VALUES
+(1, 'Janeiro'),
+(2, 'Fevereiro'),
+(3, 'Marco'),
+(4, 'Abril'),
+(5, 'Maio'),
+(6, 'Junho'),
+(7, 'Julho'),
+(8, 'Agosto'),
+(9, 'Setembro'),
+(10, 'Outubro'),
+(11, 'Novembro'),
+(12, 'Dezembro');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `paises`
+--
+
+CREATE TABLE `paises` (
+  `paisId` int(11) NOT NULL,
+  `paisNome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `paises`
+--
+
+INSERT INTO `paises` (`paisId`, `paisNome`) VALUES
+(1, 'AFEGANISTÃO'),
+(2, 'ACROTÍRI E DECELIA'),
+(3, 'ÁFRICA DO SUL'),
+(4, 'ALBÂNIA'),
+(5, 'ALEMANHA'),
+(6, 'AMERICAN SAMOA'),
+(7, 'ANDORRA'),
+(8, 'ANGOLA'),
+(9, 'ANGUILLA'),
+(10, 'ANTÍGUA E BARBUDA'),
+(11, 'ANTILHAS NEERLANDESAS'),
+(12, 'ARÁBIA SAUDITA'),
+(13, 'ARGÉLIA'),
+(14, 'ARGENTINA'),
+(15, 'ARMÉNIA'),
+(16, 'ARUBA'),
+(17, 'AUSTRÁLIA'),
+(18, 'ÁUSTRIA'),
+(19, 'AZERBAIJÃO'),
+(20, 'BAHAMAS'),
+(21, 'BANGLADECHE'),
+(22, 'BARBADOS'),
+(23, 'BARÉM'),
+(24, 'BASSAS DA ÍNDIA'),
+(25, 'BÉLGICA'),
+(26, 'BELIZE'),
+(27, 'BENIM'),
+(28, 'BERMUDAS'),
+(29, 'BIELORRÚSSIA'),
+(30, 'BOLÍVIA'),
+(31, 'BÓSNIA E HERZEGOVINA'),
+(32, 'BOTSUANA'),
+(33, 'BRASIL'),
+(34, 'BRUNEI DARUSSALAM'),
+(35, 'BULGÁRIA'),
+(36, 'BURQUINA FASO'),
+(37, 'BURUNDI'),
+(38, 'BUTÃO'),
+(39, 'CABO VERDE'),
+(40, 'CAMARÕES'),
+(41, 'CAMBOJA'),
+(42, 'CANADÁ'),
+(43, 'CATAR'),
+(44, 'CAZAQUISTÃO'),
+(45, 'CENTRO-AFRICANA REPÚBLICA'),
+(46, 'CHADE'),
+(47, 'CHILE'),
+(48, 'CHINA'),
+(49, 'CHIPRE'),
+(50, 'COLÔMBIA'),
+(51, 'COMORES'),
+(52, 'CONGO'),
+(53, 'CONGO REPÚBLICA DEMOCRÁTICA'),
+(54, 'COREIA DO NORTE'),
+(55, 'COREIA DO SUL'),
+(56, 'COSTA DO MARFIM'),
+(57, 'COSTA RICA'),
+(58, 'CROÁCIA'),
+(59, 'CUBA'),
+(60, 'DINAMARCA'),
+(61, 'DOMÍNICA'),
+(62, 'EGIPTO'),
+(63, 'EMIRADOS ÁRABES UNIDOS'),
+(64, 'EQUADOR'),
+(65, 'ERITREIA'),
+(66, 'ESLOVÁQUIA'),
+(67, 'ESLOVÉNIA'),
+(68, 'ESPANHA'),
+(69, 'ESTADOS UNIDOS'),
+(70, 'ESTÓNIA'),
+(71, 'ETIÓPIA'),
+(72, 'FAIXA DE GAZA'),
+(73, 'FIJI'),
+(74, 'FILIPINAS'),
+(75, 'FINLÂNDIA'),
+(76, 'FRANÇA'),
+(77, 'GABÃO'),
+(78, 'GÂMBIA'),
+(79, 'GANA'),
+(80, 'GEÓRGIA'),
+(81, 'GIBRALTAR'),
+(82, 'GRANADA'),
+(83, 'GRÉCIA'),
+(84, 'GRONELÂNDIA'),
+(85, 'GUADALUPE'),
+(86, 'GUAM'),
+(87, 'GUATEMALA'),
+(88, 'GUERNSEY'),
+(89, 'GUIANA'),
+(90, 'GUIANA FRANCESA'),
+(91, 'GUINÉ'),
+(92, 'GUINÉ EQUATORIAL'),
+(93, 'GUINÉ-BISSAU'),
+(94, 'HAITI'),
+(95, 'HONDURAS'),
+(96, 'HONG KONG'),
+(97, 'HUNGRIA'),
+(98, 'IÉMEN'),
+(99, 'ILHA BOUVET'),
+(100, 'ILHA CHRISTMAS'),
+(101, 'ILHA DE CLIPPERTON'),
+(102, 'ILHA DE JOÃO DA NOVA'),
+(103, 'ILHA DE MAN'),
+(104, 'ILHA DE NAVASSA'),
+(105, 'ILHA EUROPA'),
+(106, 'ILHA NORFOLK'),
+(107, 'ILHA TROMELIN'),
+(108, 'ILHAS ASHMORE E CARTIER'),
+(109, 'ILHAS CAIMAN'),
+(110, 'ILHAS COCOS (KEELING)'),
+(111, 'ILHAS COOK'),
+(112, 'ILHAS DO MAR DE CORAL'),
+(113, 'ILHAS FALKLANDS (ILHAS MALVINAS)'),
+(114, 'ILHAS FEROE'),
+(115, 'ILHAS GEÓRGIA DO SUL E SANDWICH DO SUL'),
+(116, 'ILHAS MARIANAS DO NORTE'),
+(117, 'ILHAS MARSHALL'),
+(118, 'ILHAS PARACEL'),
+(119, 'ILHAS PITCAIRN'),
+(120, 'ILHAS SALOMÃO'),
+(121, 'ILHAS SPRATLY'),
+(122, 'ILHAS VIRGENS AMERICANAS'),
+(123, 'ILHAS VIRGENS BRITÂNICAS'),
+(124, 'ÍNDIA'),
+(125, 'INDONÉSIA'),
+(126, 'IRÃO'),
+(127, 'IRAQUE'),
+(128, 'IRLANDA'),
+(129, 'ISLÂNDIA'),
+(130, 'ISRAEL'),
+(131, 'ITÁLIA'),
+(132, 'JAMAICA'),
+(133, 'JAN MAYEN'),
+(134, 'JAPÃO'),
+(135, 'JERSEY'),
+(136, 'JIBUTI'),
+(137, 'JORDÂNIA'),
+(138, 'KIRIBATI'),
+(139, 'KOWEIT'),
+(140, 'LAOS'),
+(141, 'LESOTO'),
+(142, 'LETÓNIA'),
+(143, 'LÍBANO'),
+(144, 'LIBÉRIA'),
+(145, 'LÍBIA'),
+(146, 'LISTENSTAINE'),
+(147, 'LITUÂNIA'),
+(148, 'LUXEMBURGO'),
+(149, 'MACAU'),
+(150, 'MACEDÓNIA'),
+(151, 'MADAGÁSCAR'),
+(152, 'MALÁSIA'),
+(153, 'MALAVI'),
+(154, 'MALDIVAS'),
+(155, 'MALI'),
+(156, 'MALTA'),
+(157, 'MARROCOS'),
+(158, 'MARTINICA'),
+(159, 'MAURÍCIA'),
+(160, 'MAURITÂNIA'),
+(161, 'MAYOTTE'),
+(162, 'MÉXICO'),
+(163, 'MIANMAR'),
+(164, 'MICRONÉSIA'),
+(165, 'MOÇAMBIQUE'),
+(166, 'MOLDÁVIA'),
+(167, 'MÓNACO'),
+(168, 'MONGÓLIA'),
+(169, 'MONTENEGRO'),
+(170, 'MONTSERRAT'),
+(171, 'NAMÍBIA'),
+(172, 'NAURU'),
+(173, 'NEPAL'),
+(174, 'NICARÁGUA'),
+(175, 'NÍGER'),
+(176, 'NIGÉRIA'),
+(177, 'NIUE'),
+(178, 'NORUEGA'),
+(179, 'NOVA CALEDÓNIA'),
+(180, 'NOVA ZELÂNDIA'),
+(181, 'OMÃ'),
+(182, 'PAÍSES BAIXOS'),
+(183, 'PALAU'),
+(184, 'PALESTINA'),
+(185, 'PANAMÁ'),
+(186, 'PAPUÁSIA-NOVA GUINÉ'),
+(187, 'PAQUISTÃO'),
+(188, 'PARAGUAI'),
+(189, 'PERU'),
+(190, 'POLINÉSIA FRANCESA'),
+(191, 'POLÓNIA'),
+(192, 'PORTO RICO'),
+(193, 'PORTUGAL'),
+(194, 'QUÉNIA'),
+(195, 'QUIRGUIZISTÃO'),
+(196, 'REINO UNIDO'),
+(197, 'REPÚBLICA CHECA'),
+(198, 'REPÚBLICA DOMINICANA'),
+(199, 'ROMÉNIA'),
+(200, 'RUANDA'),
+(201, 'RÚSSIA'),
+(202, 'SAHARA OCCIDENTAL'),
+(203, 'SALVADOR'),
+(204, 'SAMOA'),
+(205, 'SANTA HELENA'),
+(206, 'SANTA LÚCIA'),
+(207, 'SANTA SÉ'),
+(208, 'SÃO CRISTÓVÃO E NEVES'),
+(209, 'SÃO MARINO'),
+(210, 'SÃO PEDRO E MIQUELÃO'),
+(211, 'SÃO TOMÉ E PRÍNCIPE'),
+(212, 'SÃO VICENTE E GRANADINAS'),
+(213, 'SEICHELES'),
+(214, 'SENEGAL'),
+(215, 'SERRA LEOA'),
+(216, 'SÉRVIA'),
+(217, 'SINGAPURA'),
+(218, 'SÍRIA'),
+(219, 'SOMÁLIA'),
+(220, 'SRI LANCA'),
+(221, 'SUAZILÂNDIA'),
+(222, 'SUDÃO'),
+(223, 'SUÉCIA'),
+(224, 'SUÍÇA'),
+(225, 'SURINAME'),
+(226, 'SVALBARD'),
+(227, 'TAILÂNDIA'),
+(228, 'TAIWAN'),
+(229, 'TAJIQUISTÃO'),
+(230, 'TANZÂNIA'),
+(231, 'TERRITÓRIO BRITÂNICO DO OCEANO ÍNDICO'),
+(232, 'TERRITÓRIO DAS ILHAS HEARD E MCDONALD'),
+(233, 'TIMOR-LESTE'),
+(234, 'TOGO'),
+(235, 'TOKELAU'),
+(236, 'TONGA'),
+(237, 'TRINDADE E TOBAGO'),
+(238, 'TUNÍSIA'),
+(239, 'TURKS E CAICOS'),
+(240, 'TURQUEMENISTÃO'),
+(241, 'TURQUIA'),
+(242, 'TUVALU'),
+(243, 'UCRÂNIA'),
+(244, 'UGANDA'),
+(245, 'URUGUAI'),
+(246, 'USBEQUISTÃO'),
+(247, 'VANUATU'),
+(248, 'VENEZUELA'),
+(249, 'VIETNAME'),
+(250, 'WALLIS E FUTUNA'),
+(251, 'ZÂMBIA'),
+(252, 'ZIMBABUÉ');
 
 -- --------------------------------------------------------
 
@@ -142,6 +458,12 @@ ALTER TABLE `cliente`
   ADD PRIMARY KEY (`cliente_id`);
 
 --
+-- Indexes for table `paises`
+--
+ALTER TABLE `paises`
+  ADD PRIMARY KEY (`paisId`);
+
+--
 -- Indexes for table `produto`
 --
 ALTER TABLE `produto`
@@ -162,31 +484,38 @@ ALTER TABLE `stock`
 --
 ALTER TABLE `carrinho`
   MODIFY `carrinho_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `carrinho_stock`
 --
 ALTER TABLE `carrinho_stock`
   MODIFY `carstock_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
   MODIFY `categoria_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `cliente_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
   MODIFY `produto_id` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT for table `stock`
 --
 ALTER TABLE `stock`
   MODIFY `stock_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
